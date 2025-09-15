10e10e2a2acddb536cc7101f4c6e290160a5f267
Frank Bracq <35921662+frankbracq@users.noreply.github.com>
2025-09-15 23:15:40 +0200

    Fix: Remove deprecated load_plugin_textdomain() call - v2.1.9
    WordPress 4.6+ automatically loads translations for plugins hosted on WordPress.org
    Fixes PluginCheck warning about discouraged function

--- genealorama.php
+++ genealorama.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Secure Iframe Embed for Genealorama
  * Description: Secure iframe integration to embed the Genealorama web application into WordPress sites with dedicated page templates and credential validation
- * Version: 2.1.8
+ * Version: 2.1.9
  * Author: genealorama.com
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
@@ -43,9 +43,6 @@ class Secure_Iframe_Embed_For_Genealorama {
      * Constructeur - initialise le plugin
      */
     public function __construct() {
-        // Charger les traductions
-        add_action('init', array($this, 'load_textdomain'));
-
         // Enregistrer le shortcode
         add_shortcode('genealorama_embed', array($this, 'genealorama_shortcode'));
 
@@ -65,17 +62,6 @@ class Secure_Iframe_Embed_For_Genealorama {
         add_action('genealorama_daily_validation', array($this, 'perform_daily_validation'));
     }
     
-    /**
-     * Charger les fichiers de traduction
-     */
-    public function load_textdomain() {
-        load_plugin_textdomain(
-            'secure-iframe-embed-for-genealorama',
-            false,
-            dirname(plugin_basename(__FILE__)) . '/languages/'
-        );
-    }
-
     /**
      * Planifier la validation quotidienne des identifiants
      */
